1193758d475071784dc0b72a2292c877be973944
Minal-codezee <minal.codezee@gmail.com>
2023-04-19 11:17:24 +0530

    change version 4

--- main.js
+++ main.js
@@ -43,9 +43,20 @@ ipcMain.on("restart_app", () => {
   autoUpdater.quitAndInstall();
 });
 
+autoUpdater.logger.transports.file.level = "info";
+autoUpdater.on("checking-for-update", function (_arg1) {
+    return log.info("Checking for update...");
+});
+
 autoUpdater.on('update-available', () => {
+  console.log('update-available');
   mainWindow.webContents.send('update_available');
 });
 autoUpdater.on('update-downloaded', () => {
+  console.log('update-downloaded');
   mainWindow.webContents.send('update_downloaded');
+});
+autoUpdater.on("error", function (err) {
+  console.log('err', err);
+  return log.info("Error in auto-updater. " + err);
 });
